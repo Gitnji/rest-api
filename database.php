ec88d09c3137c6e169cce2c7941cc2ff4100a9e1
Gitnji <164335196+Gitnji@users.noreply.github.com>
2025-07-11 11:52:00 +0100

    Update database.php

--- database.php
+++ database.php
@@ -5,13 +5,11 @@ $passsword = '';
 $dbname = 'users';
 
 //create connection
-$conn = mysqli_connect($host, $username, $passsword, $dbname);
+$conn = mysqli_connect($host, $username, $password, $dbname);
 //check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 } else{
     echo "conncted successfully"
-}
-
 }
 ?>
