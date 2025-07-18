d95c598d09ca98524cbb9bc1039f72325ac23f9f
Gitnji <164335196+Gitnji@users.noreply.github.com>
2025-07-18 10:56:42 +0100

    Update database.php

--- database.php
+++ database.php
@@ -7,7 +7,7 @@ $dbname = 'users';
 
 //create connection
 try {
-    $dsn = "pgsql:host=$host;dbname=$dbname;post=$port";
+    $dsn = "pgsql:host=$host;dbname=$dbname;port=$port";
     $pdo = new PDO($dsn, $username, $password);
     // Set error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
